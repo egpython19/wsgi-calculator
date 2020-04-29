56629b0bcf1d37fbc6cefd028b3d33ecb2d22722
egpython19 <egpython19@gmail.com>
2020-04-28 18:00:48 -0700

    Commit of calculator.py to repo.

--- calculator.py
+++ calculator.py
@@ -1,57 +1,93 @@
-"""
-For your homework this week, you'll be creating a wsgi application of
-your own.
+#!/usr/bin/env python
 
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at `http://localhost:8080/multiple/3/5' then the response
-body in my browser should be `15`.
-
-Consider the following URL/Response body pairs as tests:
-
-```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
+import traceback
 
 
-"""
+def index():
+    """ root or home page - return HTML with instructions for using program """
+
+    page = """
+        <h1>Calculator Program</h1>
+        <h2>To use this program, type your desired operations into the url.</h2>
+        <h2>Here are examples:</h2>
+        <p></p>"""
+
+    for item in ['add', 'subtract', 'multiply', 'divide']:
+        page += f"""
+        <DT style='font-size:24px'>{item} 23 and 42:</DT>
+        <DD style='font-size:24px' 'margin-left:10%'>
+        <a href='http://localhost:8080/{item}/23/42'>http://localhost:8080/{item}/23/42</a></DD>
+        </DT>
+        <p></p>
+        """
+
+    return page
 
 
 def add(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """ Returns a STRING with the SUM of the arguments """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+    try:
+        total = sum(map(int, args))
+    except ValueError:
+        raise
 
-    return sum
+    main_page = '<h3><a href="http://localhost:8080">home</a><h3>'
 
-# TODO: Add functions for handling more arithmetic operations.
+    return f'<h1>The sum is: {str(total)}</h1>' \
+           f'{main_page}'
+
+
+def subtract(*args):
+    """ Returns a STRING with the DIFFERENCE of the arguments """
+
+    difference = int(args[0])
+
+    try:
+        for arg in args[1:]:
+            difference -= int(arg)
+    except ValueError:
+        raise
+
+    main_page = '<h3><a href="http://localhost:8080">home</a><h3>'
+
+    return f'<h1>The difference is: {str(difference)}</h1>' \
+           f'{main_page}'
+
+
+def multiply(*args):
+    """ Returns a STRING with the PRODUCT of the arguments """
+
+    product = int(args[0])
+
+    try:
+        for arg in args[1:]:
+            product *= int(arg)
+    except ValueError:
+        raise
+
+    main_page = '<h3><a href="http://localhost:8080">home</a><h3>'
+
+    return f'<h1>The product is: {str(product)}</h1>' \
+           f'{main_page}'
+
+
+def divide(*args):
+    """ Returns a STRING with the quotient of the arguments """
+
+    quotient = int(args[0])
+
+    try:
+        for arg in args[1:]:
+            quotient /= int(arg)
+    except (ValueError, ZeroDivisionError):
+        raise
+
+    main_page = '<h3><a href="http://localhost:8080">home</a><h3>'
+
+    return f'<h1>The quotient is: {str(quotient)}</h1>' \
+           f'{main_page}'
+
 
 def resolve_path(path):
     """
@@ -59,26 +95,59 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '': index,
+        'add': add,
+        'multiply': multiply,
+        'subtract': subtract,
+        'divide': divide
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    """ wsgi conforming application class """
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = '200 OK'
+    except NameError:
+        status = '404 Not Found'
+        body = '<h1>Not Found</h1>'
+    except ValueError:
+        status = '406 Not Acceptable'
+        body = '<h1>Not Acceptable</h1>' \
+               '<h2>Attempted math operation on a non-number</h2>'
+    except ZeroDivisionError:
+        status = '406 Not Acceptable'
+        body = '<h1>Not Acceptable</h1>' \
+               '<h2>Attempted division by zero</h2>'
+    except Exception:
+        status = '500 Internal Server Error'
+        body = '<h1>Internal Server Error</h1>'
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
+
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
